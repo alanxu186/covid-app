bbcd825db03482cc7f422f5dacc81c70c62d7ee1
Alan Xu <alan.xu1699@gmail.com>
2022-11-24 20:30:13 -0500

    successfully fetched from covid api

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import {Routes, Route} from 'react-router-dom'
 import AboutPage from './components/AboutPage'
 import HomePage from './components/HomePage'
+import CovidData from "./CovidAPI/CovidData";
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-3xl underline font-bold text-red-700'>Test tailwindcss</h1>
+      <CovidData/>
     </div>
   );
 }
--- src/CovidAPI/CovidData.js
+++ src/CovidAPI/CovidData.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const CovidData = () => {
+    const [country, setCountry] = useState('');
+    const [cases, setCases] = useState('')
+    const [recovered, setRecovered] = useState('')
+    const [deaths, setDeaths] = useState('')
+    const [todayCases, setTodayCases] = useState('')
+    const [recoveredCases, setRecoveredCases] = useState('')
+    const [userInput, setUserInput] = useState('')
+
+    useEffect(() => {
+        fetch('https://disease.sh/v3/covid-19/countries')
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [])
+
     return (
         <div>CovidData</div>
     )
