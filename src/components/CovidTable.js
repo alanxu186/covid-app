9c8e808cc828b17b907488c8a86a51966a748720
Alan Xu <alan.xu1699@gmail.com>
2022-11-27 21:28:40 -0500

    created line graph, map, covidtable components, messed with search bar css

---
+++ src/components/CovidTable.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CovidTable = () => {
+    return (
+        <div>CovidTable</div>
+    )
+}
+
+export default CovidTable
---
+++ src/components/LineGraph.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LineGraph = () => {
+    return (
+        <div>LineGraph</div>
+    )
+}
+
+export default LineGraph
