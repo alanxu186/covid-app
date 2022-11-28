38d196151f9f97ea6d7fdbc0b9fdb07f524839fc
Alan Xu <alan.xu1699@gmail.com>
2022-11-27 21:35:21 -0500

    install react-icons package, create navbar, create routes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router-dom'
 import AboutPage from './components/AboutPage'
 import HomePage from './components/HomePage'
 import CovidData from "./components/CovidData";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <h1 className='text-3xl underline font-bold text-red-700'>Test tailwindcss</h1>
+      <Navbar />
       <CovidData />
     </div>
   );
