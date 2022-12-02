94736ec7f6fd952db70ef59f3ce6d1e42327a78c
Alan Xu <alan.xu1699@gmail.com>
2022-12-01 23:51:24 -0500

    set states in app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from "react";
-import { Routes, Route } from 'react-router-dom'
-import AboutPage from './components/AboutPage'
-import HomePage from './components/HomePage'
+import React, {useState, useEffect} from "react";
+import { Routes, Route } from 'react-router-dom';
+import AboutPage from './components/AboutPage';
+import HomePage from './components/HomePage';
 import CovidData from "./components/CovidData";
+import Map from "./components/Map";
+import CovidTable from "./components/CovidTable";
+import InfoBox from "./components/InfoBox";
+import LineGraph from "./components/LineGraph";
 import Navbar from "./components/Navbar";
+import "leaflet/dist/leaflet.css"
 
 function App() {
+  
+  // dropdown feature
+  const [countries, setCountries] = useState([]); 
+  // store selected country name
+  const [country, setCountry] = useState('worldwide');
+  // store country info
+  const [countryInfo, setCountryInfo] = useState({});
+  // store data of total covid cases for each country
+  const [tableData, setTableData] = useState([]);
+  // store latitude and longitude value of selected country
+  const [mapCenter, setMapCenter] = useState({lat:34.80746, lng:-40.4796});
+  // zoom in and zoom out of map
+  const [mapZoom, setMapZoom] = useState(3);
+  // store array of list of countries present in map
+  const [mapCountries, setMapCountries] = useState([]);
+  // style components based on cases, recovered, deaths 
+  const [caseTypes, setCaseTypes] = useState('cases');
+
+  //fetch from covid api
+  useEffect(() => { 
+    fetch('https://disease.sh/v3/covid-19/all')
+    .then(res => res.json())
+    .then(data => 
+      setCountryInfo(data))
+  },[])
+
   return (
     <div className="App">
       <Navbar />
-      <CovidData />
+      {/* <CovidData /> */}
     </div>
   );
 }
