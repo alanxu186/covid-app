1ea8b0a934dbc3515ae8de141c0813943458b4d4
Alan Xu <alan.xu1699@gmail.com>
2022-12-06 21:11:34 -0500

    install material ui, add useEffect to fetch individual countryies, and created onChange function for dropdown menu

--- src/App.js
+++ src/App.js
@@ -37,24 +37,49 @@ function App() {
         setCountryInfo(data))
   }, [])
 
+  //fetch individual country info
   useEffect(() => {
     const getCountriesData = async () => {
       await fetch('https://disease.sh/v3/covid-19/countries')
         .then((res) => res.json())
         .then((data) => {
+          //store country's name and value
           const countries = countries.map((country) => (
             {
               name: country.country,
               value: country.countryInfo.iso2
             }
           ));
+          // sort data in descending order and setting it to tabledData
           const sortedData = sortData(data)
           setTableData(sortedData);
+          // data response passed to mapCountries to be used in map component
           setMapCountries(data);
           setCountries(countries)
         })
     }
-  })
+    getCountriesData
+  }, [])
+
+  // when clicking dropdown, set the value of the selected country and fetch for its info
+  const onCountryChange = async (e) => {
+    const countryCode = e.target.value;
+    setCountry(countryCode);
+    // if countryCode matches worldwide then return fetch for all, else return the specific country code
+    const url = countryCode === 'worldwide' ? 'https://disease.sh/v3/covid-19/all' : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+    await fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        setCountry(countryCode);
+        setCountryInfo(data);
+        // set the specific latitude and longitude of the selected country
+        setMapCenter([data.countryInfo.lat, data.countryInfo.long])
+        // magnify by 4x when country is selected
+        setMapZoom(4);
+      })
+  }
+
+
 
   return (
     <div className="App">
