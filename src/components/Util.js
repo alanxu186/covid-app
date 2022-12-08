39b1f74400aea5f09eb92d2b7d8889eda5d038af
Alan Xu <alan.xu1699@gmail.com>
2022-12-07 23:40:21 -0500

    set up hexcode colors for case types and sorting data function

--- src/components/Util.js
+++ src/components/Util.js
@@ -2,11 +2,46 @@ import React from 'react';
 import { Circle, Popup } from 'react-leaflet';
 import numeral from 'numeral'
 
-const Util = () => {
-
-    return (
-        <div>Util</div>
-    )
+const casesTypeColors = {
+    cases: {
+        hex: "#CC1034",
+        multiplier: 800
+    },
+    recovered: {
+        hex: "#7dd71d",
+        multiplier: 1200
+    },
+    deaths: {
+        hex: "#fb4443",
+        multiplier: 2000
+    }
 }
 
-export default Util
+export const sortData = (data) => {
+    const sortedData = [...data];
+    sortedData.sort((a, b) => {
+        if (a.cases > b.cases) {
+            return -1;
+        } else {
+            return 1
+        }
+    })
+    return sortedData
+}
+
+export const showDataOnMap = (data, casesType = 'cases') => {
+    data.map(country => (
+        <Circle center={[country.countryInfo.lat,country.countryInfo.long]}
+        fillOpacity={0.4}
+        color={casesTypeColors[casesType].hex}
+        fillColor={casesTypeColors[casesType].hex}
+        radius={
+            Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+        }
+        >
+            <Popup>
+
+            </Popup>
+        </Circle>
+    ))
+}
