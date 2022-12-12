cc10d8df45d6c1ab88afc971cc5b36573f9360dd
Alan Xu <alan.xu1699@gmail.com>
2022-12-11 22:50:14 -0500

    imported map container and tilelayer component from react-leaflet, set up react-leaflet

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import '../stylesheets/Map.css';
-import { Map as LeafletMap, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import { showDataOnMap } from './Util';
 
 const Map = ({ countries, casesType, center, zoom }) => {
 
-    console.log(countries)
-
     return (
-        <div>map of countries</div>
+        <div className='map'>
+            <MapContainer center={center} zoom={zoom}>
+                <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'>
+                </TileLayer>
+                {showDataOnMap(countries,casesType)}
+            </MapContainer>
+
+        </div>
     )
 }
 
