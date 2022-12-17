00e6cc7dc8718e874931a75750b2a8b48a938757
Alan Xu <alan.xu1699@gmail.com>
2022-12-16 22:47:50 -0500

    set up first half of display option

--- src/components/LineGraph.js
+++ src/components/LineGraph.js
@@ -1,12 +1,26 @@
-import React, {useState, useEffect} from 'react';
-import {Line} from 'react-chartjs-2';
+import React, { useState, useEffect } from 'react';
+import { Line } from 'react-chartjs-2';
 import numeral from 'numeral';
 import { useState } from 'react';
 
-const LineGraph = () => {
-    return (
-        <div>LineGraph</div>
-    )
+//set display options 
+const options = {
+    legend: {
+        display: false,
+    },
+    elements: {
+        point: {
+            radius: 0,
+        },
+    },
+    maintainAspectRatio: false,
+    tooltips: {
+        mode: "index",
+        intersect: false,
+        callbacks: {
+            label: function (tooltipItem, data) {
+                return numeral(tooltipItem.value).format("+0,0");
+            }
+        }
+    }
 }
-
-export default LineGraph
